68dfbe2ed7e47c628fecb584ee2e9dd42d65dc16
Enoch Klu <eenoch@delalify.com>
2026-04-05 18:06:56 +0000

    feat: create Result type to represent sucess and failure values

--- apps/backend/src/libraries/result.ts
+++ apps/backend/src/libraries/result.ts
@@ -1,49 +1,49 @@
 class OkResult<T, E = never> {
-	#data: T;
+  #data: T
 
-	constructor(data: T) {
-		this.#data = data;
-	}
+  constructor(data: T) {
+    this.#data = data
+  }
 
-	get data() {
-		return this.#data;
-	}
+  get data() {
+    return this.#data
+  }
 
-	isOk(): this is OkResult<T, E> {
-		return true;
-	}
+  isOk(): this is OkResult<T, E> {
+    return true
+  }
 
-	isErr(): this is ErrResult<T, E> {
-		return false;
-	}
+  isErr(): this is ErrResult<T, E> {
+    return false
+  }
 }
 
 class ErrResult<T, E> {
-	#error: E;
+  #error: E
 
-	constructor(error: E) {
-		this.#error = error;
-	}
+  constructor(error: E) {
+    this.#error = error
+  }
 
-	get error() {
-		return this.#error;
-	}
+  get error() {
+    return this.#error
+  }
 
-	isOk(): this is OkResult<T, E> {
-		return false;
-	}
+  isOk(): this is OkResult<T, E> {
+    return false
+  }
 
-	isErr(): this is ErrResult<T, E> {
-		return true;
-	}
+  isErr(): this is ErrResult<T, E> {
+    return true
+  }
 }
 
 export function Ok<T>(value: T) {
-	return new OkResult(value);
+  return new OkResult(value)
 }
 
 export function Err<E>(error: E) {
-	return new ErrResult(error);
+  return new ErrResult(error)
 }
 
-export type Result<T, E> = OkResult<T, E> | ErrResult<T, E>;
+export type Result<T, E> = OkResult<T, E> | ErrResult<T, E>
