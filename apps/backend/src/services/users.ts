09222aa003430adb853f236a7b71aa99b08068ac
Enoch Klu <eenoch@delalify.com>
2026-04-05 18:16:20 +0000

    chore: move Biome config to root

--- apps/backend/src/services/users.ts
+++ apps/backend/src/services/users.ts
@@ -1,10 +1,7 @@
-import type { CreateOneUserType } from "../dtos/users";
-import { Ok, type Result } from "../libraries/result";
-import type { ServiceHandler } from "../types/global";
+import type { CreateOneUserType } from "../dtos/users"
+import { Ok, type Result } from "../libraries/result"
+import type { ServiceHandler } from "../types/global"
 
-export const $createOneUser: ServiceHandler<
-	CreateOneUserType,
-	Result<boolean, unknown>
-> = (payload) => {
-	return Ok(true);
-};
+export const $createOneUser: ServiceHandler<CreateOneUserType, Result<boolean, unknown>> = (_payload) => {
+  return Ok(true)
+}
