003383a06214d4c0cf2fc64ef75715471bc6a8d9
Enoch Klu <eenoch@delalify.com>
2026-04-05 18:08:12 +0000

    chore: create types for global usage

---
+++ apps/backend/src/types/global.ts
@@ -0,0 +1,15 @@
+import type { Env, Handler, Input } from "hono";
+import type { BlankInput, HandlerResponse } from "hono/types";
+import type { Result } from "../libraries/result";
+
+export type ApiHandler<
+	Data extends Env["Variables"] = any,
+	P extends string = any,
+	I extends Input = BlankInput,
+	R extends HandlerResponse<any> = any,
+> = Handler<{ Variables: { payload: Data } }, P, I, R>;
+
+export type ServiceHandler<
+	Payload = any,
+	Output extends Result<any, unknown> = Result<any, unknown>,
+> = (payload: Payload) => Output;
