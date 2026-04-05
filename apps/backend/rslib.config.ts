5a12b6b92c2f1ce217d545702d89d80720f857a6
Enoch Klu <eenoch@delalify.com>
2026-04-05 18:10:48 +0000

    chore: format configs

--- apps/backend/rslib.config.ts
+++ apps/backend/rslib.config.ts
@@ -1,47 +1,47 @@
-import { defineConfig } from "@rslib/core";
+import { defineConfig } from "@rslib/core"
 
 export default defineConfig({
-	resolve: { extensions: [".ts", ".mjs", ".js", ".json"] },
-	source: {
-		define: {
-			"process.env.NODE_ENV": '"production"',
-		},
-	},
-	output: {
-		cleanDistPath: true,
-		minify: true,
-		sourceMap: {
-			js: "cheap-module-source-map",
-		},
-	},
-	lib: [
-		{
-			format: "esm",
-			syntax: "esnext",
-			bundle: true,
-			autoExtension: false,
-			source: {
-				entry: {
-					app: "./src/index.ts",
-				},
-				tsconfigPath: "./tsconfig.json",
-			},
-			output: {
-				distPath: {
-					root: "./build",
-				},
-				legalComments: "none",
-			},
-			tools: {
-				rspack: {
-					target: ["node20"],
-					optimization: {
-						nodeEnv: "production",
-						sideEffects: false,
-						mangleExports: "size",
-					},
-				},
-			},
-		},
-	],
-});
+  resolve: { extensions: [".ts", ".mjs", ".js", ".json"] },
+  source: {
+    define: {
+      "process.env.NODE_ENV": '"production"',
+    },
+  },
+  output: {
+    cleanDistPath: true,
+    minify: true,
+    sourceMap: {
+      js: "cheap-module-source-map",
+    },
+  },
+  lib: [
+    {
+      format: "esm",
+      syntax: "esnext",
+      bundle: true,
+      autoExtension: false,
+      source: {
+        entry: {
+          app: "./src/index.ts",
+        },
+        tsconfigPath: "./tsconfig.json",
+      },
+      output: {
+        distPath: {
+          root: "./build",
+        },
+        legalComments: "none",
+      },
+      tools: {
+        rspack: {
+          target: ["node20"],
+          optimization: {
+            nodeEnv: "production",
+            sideEffects: false,
+            mangleExports: "size",
+          },
+        },
+      },
+    },
+  ],
+})
