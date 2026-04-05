4d56ad627450d3b0a42586b2a71aa91d844a99b9
Enoch Klu <eenoch@delalify.com>
2026-04-05 18:07:53 +0000

    feat: create initial controller, DTO, and service handler for user management

---
+++ apps/backend/src/controllers/users.ts
@@ -0,0 +1,14 @@
+import type { CreateOneUserType } from "../dtos/users";
+import { $createOneUser } from "../services/users";
+import type { ApiHandler } from "../types/global";
+import { sendResponse } from "../utilities/api";
+
+export const handleCreateOneUser: ApiHandler<CreateOneUserType> = (c) => {
+	const result = $createOneUser(c.get("payload"));
+
+	if (result.isOk()) {
+		return sendResponse(c, { status: 201, message: "OK" });
+	}
+
+	return sendResponse(c, { status: 500, message: "Hmm..." });
+};
---
+++ apps/backend/src/dtos/users.ts
@@ -0,0 +1,7 @@
+import * as v from "valibot";
+
+export const CreateOneUser = v.object({
+	first_name: v.pipe(v.string()),
+});
+
+export type CreateOneUserType = v.InferOutput<typeof CreateOneUser>;
